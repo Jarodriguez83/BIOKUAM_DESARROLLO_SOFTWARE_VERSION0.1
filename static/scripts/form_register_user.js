9affb186111d3ca31d8abb3ce5e25be99198c34b
Jarodriguez83 <jarodriguez83@ucatolica.edu.co>
2026-01-17 22:16:48 -0500

    SCRIPT FORM REGISTER

--- static/scripts/form_register_user.js
+++ static/scripts/form_register_user.js
@@ -1,8 +1,9 @@
-console.log("Script cargado correctamente");
 
-// Esta función detecta cualquier intento de recarga
+console.log("EL SCRIPT DE REGISTRO DE USUARIO ESTÁ FUNCIONANDO CORRECTAMENTE.");
+
+//DETECTA CUANDO LA PÁGINA INTENTA RECARGARSE
 window.onbeforeunload = function() {
-    console.warn("¡La página está intentando recargarse!");
+    console.warn("¡LA PÁGINA ESTA INTENTANDO RECARGARSE!");
 };
 document.getElementById('formRegistro').addEventListener('submit', async function(e) {
     e.preventDefault();  //DETENER LA RECARGA DE LA PÁGINA
@@ -17,12 +18,13 @@ document.getElementById('formRegistro').addEventListener('submit', async functio
             apellidos: document.getElementById('reg_apellidos').value,
             telefono: document.getElementById('reg_telefono').value,
             correo: document.getElementById('reg_correo').value,
-            tipo_documento: document.getElementById('reg_tipo_doc').value,
-            numero_documento: document.getElementById('reg_num_doc').value,
+            tipo_identificacion: document.getElementById('reg_tipo_doc').value,
+            numero_identificacion: document.getElementById('reg_num_doc').value,
             vereda: document.getElementById('reg_vereda').value,
             nombre_finca: document.getElementById('reg_finca').value || "SIN NOMBRE",  
             folio_finca: document.getElementById('reg_folio').value || "SIN FOLIO",
             referencia_prototipo: document.getElementById('reg_prototipo').value,
+            crear_contrasena: document.getElementById('reg_crear_pass').value,
             contrasena: document.getElementById('reg_pass').value,
             foto_perfil: reader.result //CONVERTIR LA FOTO A BASE64
         };  
@@ -36,10 +38,14 @@ document.getElementById('formRegistro').addEventListener('submit', async functio
             });  
             const resultado = await repuesta.json();
             if (repuesta.ok) {
-                //GUARDAR EL ID EN LOCALSTORAGE
+                //GUARDAR EL ID Y OTROS DATOS EN LOCALSTORAGE
                 localStorage.setItem('usuario_id_biokuam', resultado.usuario_id);
-                alert('REGISTRO EXITOSO');
-                window.location.href = 'inicio.html'; //REDIRIGIR A LA PÁGINA PRINCIPAL
+                localStorage.setItem('usuario_nombre_biokuam', datosUsuario.nombres);
+                localStorage.setItem('usuario_apellido_biokuam', datosUsuario.apellidos)
+                const nombreUsuario = document.getElementById('reg_nombres').value;
+                const apellidoUsuario = document.getElementById('reg_apellidos').value;  
+                alert(`¡EL REGISTRO SE REALIZO DE FORMA CORRECTA, BIENVENIDO A BIOKUAM ${nombreUsuario} ${apellidoUsuario}!`);
+                window.location.href = '/'; //REDIRIGIR A LA PÁGINA PRINCIPAL
             } else {
                 alert('ERROR EN EL REGISTRO: ' + JSON.stringify(resultado.detail));
             }
