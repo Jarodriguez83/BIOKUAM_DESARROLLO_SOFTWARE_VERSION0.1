ec828e82d94e9f63bcff8646f6f09606c65d4dda
Jarodriguez83 <jarodriguez83@ucatolica.edu.co>
2026-01-21 15:52:53 -0500

    SCRIPTS FORM LOGIN USER

--- static/scripts/form_login_user.js
+++ static/scripts/form_login_user.js
@@ -1,22 +1,21 @@
-// Asegúrate de que este bloque esté dentro de tu archivo JS actual
 const formLogin = document.getElementById('formLogin');
 
 if (formLogin) {
-    console.log("Escuchador de Login activado correctamente.");
+    console.log("EL SCRIPT PARA EL LOGIN DEL USUARIO HA SIDO CARGADO EXITOSAMENTE");
 
     formLogin.addEventListener('submit', async function(e) {
         e.preventDefault();
 
-        // Buscamos los elementos específicamente dentro del formulario actual
+        // BUSCAR LOS ELEMENTOS ESPECIFICAMENTE DENTRO DEL FORMULARIO 
         const formulario = e.target;
         const inputUsuario = formulario.querySelector('#login_usuario');
         const inputPass = formulario.querySelector('#login_pass');
 
-        // Capturamos los valores
+        // CAPTURA DE LOS VALORES
         const correoVal = inputUsuario.value.trim();
         const passVal = inputPass.value.trim();
 
-        // --- VERIFICACIÓN CRÍTICA EN CONSOLA DEL NAVEGADOR ---
+        // VERIFICACIÓN CRÍTICA EN CONSOLA DEL NAVEGADOR
         console.log("CAPTURA JS -> Usuario:", correoVal);
         console.log("CAPTURA JS -> Pass:", passVal);
 
@@ -42,7 +41,7 @@ if (formLogin) {
             const resultado = await respuesta.json();
 
             if (respuesta.ok) {
-                // Guardar datos en el LocalStorage
+                // GUARDAR LOS DATOS EN EL LOCAL STORAGE
                 localStorage.setItem('usuario_id_biokuam', resultado.usuario_id);
                 localStorage.setItem('usuario_nombres_biokuam', resultado.nombres);
                 
@@ -55,12 +54,12 @@ if (formLogin) {
                 // Redirigir al home o perfil
                 window.location.href = '/home'; 
             } else {
-                // Esto mostrará el 401 (Clave mal) o 404 (No existe)
+                // SE MOSTRARA EL ERROR 401 (CLAVE INCORRECTA) O EL ERROR 400 (NO EXISTE).
                 alert(`ERROR: ${resultado.detail}`);
             }
         } catch (error) {
-            console.error("Error al conectar con FastAPI:", error);
-            alert("No se pudo conectar con el servidor.");
+            console.error("ERROR AL CONECTAR CON FASTAPI:", error);
+            alert("NO SE PUDO CONECTAR CON EL SERVIDOR.");
         }
     });
 }
