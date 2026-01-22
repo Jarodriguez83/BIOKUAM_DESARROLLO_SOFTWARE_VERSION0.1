d47a9d1f1f04b721b72ce48eab0b0e052df4e8bf
Jarodriguez83 <jarodriguez83@ucatolica.edu.co>
2026-01-21 22:39:06 -0500

    SCRIPTS PERFIL DATES PHOTO

--- static/scripts/script_perfil.js
+++ static/scripts/script_perfil.js
@@ -26,13 +26,13 @@ document.addEventListener('DOMContentLoaded', async () => {
                 imgPerfil.src = datos.foto_perfil;
             }
             const mensajeRefBarcos = {
-                "BKM-REF-001": "Prototipo BKM-001 asignado: Sistema de monitoreo de humedad y temperatura para cultivos en invernadero.",
-                "BKM-REF-002": "Prototipo BKM-002 asignado: Sistema de riego automatizado basado en sensores de humedad del suelo.",
-                "BKM-REF-003": "Prototipo BKM-003 asignado: Plataforma de análisis de datos agrícolas con IA para optimización de cultivos.",
+                "BKM-REF-001": "VINCULADO AL PROTOTIPO 'BKM-REF-001': Este prototipo es la puerta de entrada a la robótica acuática de BIOKUAM. Diseñado para fines educativos y recreativos, el BKM-001 ofrece un sistema de navegación eficiente controlado vía Bluetooth. Cuenta con la potencia del microcontrolador ATmega328P y un sistema de propulsión por motorreductores, ideal para aprender los fundamentos de la dinámica de fluidos y control remoto en entornos controlados.",
+                "BKM-REF-002": "VINCULADO AL PROTOTIPO 'BKM-REF-002': El modelo intermedio BKM-002 eleva las capacidades del prototipo base al integrar monitoreo ambiental activo. Equipado con un sensor de temperatura de alta precisión y alimentado por una Power Bank para mayor autonomía, este modelo permite la recolección de datos térmicos en tiempo real. Incluye un empaque reforzado y manual de protocolos, siendo la herramienta perfecta para estudios básicos de calidad de agua.",
+                "BKM-REF-003": "VINCULADO AL PROTOTIPO 'BKM-REF-003': La versión BKM-003 es nuestra estación de investigación móvil más completa y potente. Integra un ecosistema de sensores de pH y temperatura, junto a una cámara para inspección visual detallada. Su conectividad dual (WiFi/Bluetooth) permite el control a larga distancia y la gestión de datos mediante una aplicación de telemetría avanzada. Este kit profesional incluye una caja de herramientas técnica y manual de laboratorio, diseñado para misiones de monitoreo ambiental riguroso y análisis de ecosistemas acuáticos complejos.",
             };  
             const elementoDescripcion = document.getElementById('descriptionBKM');
             if (elementoDescripcion) {
-                const mensajeparaBarcos = mensajeRefBarcos[datos.referencia_prototipo] || "Prototipo asignado sin descripción específica0.";
+                const mensajeparaBarcos = mensajeRefBarcos[datos.referencia_prototipo] || "PROTOTIPO ASIGNADO SIN DESCRIPCIÓN ESPECÍFICA.";
                 elementoDescripcion.textContent = mensajeparaBarcos;
             }
         }
