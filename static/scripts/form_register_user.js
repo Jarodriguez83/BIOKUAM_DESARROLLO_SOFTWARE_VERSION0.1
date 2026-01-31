36b59974de5e27cd709f6f9c08f350de3fea6610
Jarodriguez83 <jarodriguez83@ucatolica.edu.co>
2026-01-30 22:50:08 -0500

    SCRIPT FORM REGISTER

--- static/scripts/form_register_user.js
+++ static/scripts/form_register_user.js
@@ -12,6 +12,21 @@ document.getElementById('formRegistro').addEventListener('submit', async functio
     // URL POR DEFECTO
     let fotoPublicUrl = "https://cdn-icons-png.flaticon.com/512/149/149071.png"; 
 
+    const pass1 = document.getElementById('reg_crear_pass').value;
+        const pass2 = document.getElementById('reg_pass').value;
+            if (pass1 !== pass2) {
+                alert("LAS CONTRASEÑAS NO COINCIDEN. VERIFICA E INTENTA DE NUEVO.");
+                //LIMPIAR EL CAMPO DE CONFIRMACIÓN 
+                document.getElementById('reg_pass').value = "";
+                document.getElementById('reg_pass').focus();
+                return;
+            }
+            if (pass1.length < 6) {
+                alert("LA CONTRASEÑA DEBE CONTENER AL MENOS 6 CARACTERES.");
+                document.getElementById('reg_crear_pass').focus();
+                return;
+            }
+            
     try {
         // --- PASO 1: SUBIR A SUPABASE (Si hay foto) ---
         if (fotoFile) {
@@ -53,12 +68,11 @@ document.getElementById('formRegistro').addEventListener('submit', async functio
             folio_finca: document.getElementById('reg_folio').value || "SIN FOLIO",
             referencia_prototipo: document.getElementById('reg_prototipo').value,
             
-            // AGREGAMOS EL CAMPO QUE FALTA (Debe ser igual al de tu modelo Python)
             crear_contrasena: document.getElementById('reg_crear_pass').value, 
-            
             contrasena: document.getElementById('reg_pass').value,
             foto_perfil: fotoPublicUrl 
         };
+        
         console.log("JSON final enviado:", JSON.stringify(datosUsuario));
         const repuesta = await fetch('/registration', {
             method: 'POST',
